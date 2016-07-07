44d696a90890116c55415b0314408ea5d6a4953d
AnglusWang <AnglusWang@163.com>
2016-07-07 22:14:39 +0800

    新建一个Activity活动，并在java代码中隐藏标题

--- ActivityTest/app/src/main/java/com/angluswang/raiseweapon/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/angluswang/raiseweapon/activitytest/FirstActivity.java
@@ -2,7 +2,10 @@ package com.angluswang.raiseweapon.activitytest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstActivity extends Activity {
 
@@ -11,5 +14,15 @@ public class FirstActivity extends Activity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);  //隐藏标题
         setContentView(R.layout.layout_first);
+
+       Button btn1 = (Button) findViewById(R.id.btn_1);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FirstActivity.this, "You clicked Button 1",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
