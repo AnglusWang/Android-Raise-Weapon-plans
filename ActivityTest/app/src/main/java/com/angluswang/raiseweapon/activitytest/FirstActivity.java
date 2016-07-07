b502b8c62123eb6dd63a0a0997a69c74f14fe744
AnglusWang <AnglusWang@163.com>
2016-07-07 22:39:49 +0800

    在活动中使用 Menu

--- ActivityTest/app/src/main/java/com/angluswang/raiseweapon/activitytest/FirstActivity.java
+++ ActivityTest/app/src/main/java/com/angluswang/raiseweapon/activitytest/FirstActivity.java
@@ -2,6 +2,8 @@ package com.angluswang.raiseweapon.activitytest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
@@ -24,5 +26,38 @@ public class FirstActivity extends Activity {
             }
         });
 
+    }
+
+    /**
+     * 创建菜单
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    /**
+     * 菜单被选择时，进行相应的操作（此处为吐司）
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.add_item:
+                Toast.makeText(this, "You clicked Add",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.remove_item:
+                Toast.makeText(this, "You clicked Remove",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
     }
 }
